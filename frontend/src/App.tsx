f439008e97e0b62e6d67acfbe7c71f29ff9e484a
Moyise <moyisemr@gmail.com>
2021-03-14 05:03:15 -0400

    added login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route path="/" exact />
+          <Route path="/login" exact>
+            <LoginScreen />
+          </Route>
         </Switch>
       </Router>
     </>
